7957688fe0ddfa91d6701b9eaed9abafdc87f4b3
calvean <106763858+calvean@users.noreply.github.com>
2022-06-30 23:07:11 +0200

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ int main(void)
 	printf("sizeof an int:%lu byte(s)\n", (unsigned long) sizeof(a));
 	printf("sizeof of a long int:%lu byte(s)\n",(unsigned long) sizeof(b));
 	printf("sizeof a long long int:%lu byte(s)\n",(unsigned long) sizeof(c));
-	prinff("sizeof a float:%lu byte(s)\n",(unsigned long) sizeof(f));
+	printf("sizeof a float:%lu byte(s)\n",(unsigned long) sizeof(f));
 	return (0);
 }

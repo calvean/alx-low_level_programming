321962a38f7c6dda6ff1bb58c298b112ed577a02
calvean <garnetsharara@gmail.com>
2022-06-30 10:54:28 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints size of memory allocated
+ *
+ * Return: 0 if program is sucessful
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+
+	printf("size of a char: %zu byte(s)", sizeof(charType));
+	printf("size of a int : %zu byte(s)", sizeof(intType));
+	printf("size of a long int : %zu byte(s)", sizeof(longintType));
+	printf("size of a long long int : %zu byte(s)", sizeof(longlongint));
+	printf("size of a float : %zu byte(s)", sizeof(floatType));
+	return (0);
+}

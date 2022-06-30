637cf7c025452f22759a45524f2de388215214a8
calvean <garnetsharara@gmail.com>
2022-06-30 09:36:42 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main (void)
 {
-	char Mystring[] = "Programming is like building a multilingual puzzle";
+	char Mystring[] = "\"Programming is like building a multilingual puzzle";
 	puts(Mystring);
 	return (0);
 }

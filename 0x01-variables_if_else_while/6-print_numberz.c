69e58bc8cd1a850799b185e164cf7f6d3c224588
calvean <garnetsharara@gmail.com>
2022-07-01 14:53:34 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 0-10
+ *
+ * Return: 0 if sucess
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 57; ch++)
+		putchar(ch);
+	putchar(ch);
+	return (0);
+}

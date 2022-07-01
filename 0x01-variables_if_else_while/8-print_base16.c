0c443eae065de372c7381cfee597026b76fc44e0
calvean <garnetsharara@gmail.com>
2022-07-01 15:08:23 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers to base 16
+ *
+ * Return: 0 if sucess
+ */
+int main(void)
+{
+	int ch;
+	int n;
+
+	for (n = 48; n < 57; n++)
+		putchar(n);
+	for (ch = 'a'; ch <= f; ch++)
+		putchar(ch);
+	putchar(10);
+	return (0);
+}

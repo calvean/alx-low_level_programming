f03645398ac7780f94345951c73a5dbfd154b8e9
calvean <garnetsharara@gmail.com>
2022-07-01 14:48:22 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 0-10
+ *
+ * Return: 0 if sucess
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}

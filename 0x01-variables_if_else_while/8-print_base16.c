c06c67cebb56f263ca297ff2f96ff7664fb03b2f
calvean <garnetsharara@gmail.com>
2022-07-01 18:52:02 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,10 @@ int main(void)
 	int ch;
 	int n;
 
-	for (n = 48; n < 57; n++)
+	for (n = 48; n <= 57; n++)
 		putchar(n);
 	for (ch = 'a'; ch <= 'f'; ch++)
 		putchar(ch);
+	putchar(10);
 	return (0);
 }

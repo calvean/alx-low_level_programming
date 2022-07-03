3ac48a646d8ce9c7756d6a6fac15bfa6a79442b3
calvean <garnetsharara@gmail.com>
2022-07-03 04:01:21 -0700

    comb4

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,45 +1,51 @@
 #include <stdio.h>
 /**
- * main - prints combinations
+ * main - prints combination of 4
  *
- * Return: 0 if sucess
+ * Return: 0 always
  */
 int main(void)
 {
-	int i;
-	int j;
-	int k;
-	int m;
-	int op1;
-	int op2;
-	/*my code*/
-	for (i = 48; i <= 57; i++)
+	int c, d, e, f, op1, op2;
+
+	c = d = e = f = 48;
+	while (f < 48)
 	{
-		for (j = 48; j <= 57; j++)
+		e = 48;
+		while (e < 58)
 		{
-			for (k = 48; k <= 57; k++)
+			d = 48;
+			while (d < 58)
 			{
-				for (m = 48; m <= 57; m++)
+				c = 48;
+				while (c < 58)
 				{
-					op1 = (i * 10) + j;
-					op2 = (k * 10) + m;
+					op1 = (f * 10) + e;
+					op2 = (d * 10) + c;
 					if (op1 < op2)
 					{
-						putchar(i);
-						putchar(j);
-						putchar(32);
-						putchar(k);
-						putchar(m);
-						
-						if (i == 57 && j == 56 && k == 57 && m == 56)
+						putchar(f);
+						putchar(e);
+						putchar(' ');
+						putchar(d);
+						putchar(c);
+						if (f == 57 && e == 56 && d == 57 && c == 57)
+						{
 							break;
-						/*prtint space*/
-						putchar(44);
-						putchar(32);
+						}
+						putchar(',');
+						putchar(' ');
 					}
+					c++;
 				}
+				d++;
 			}
+			e++;
 		}
+		f++;
 	}
+	putchar('\n');
+
 	return (0);
 }
+

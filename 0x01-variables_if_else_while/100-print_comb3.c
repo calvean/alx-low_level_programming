46735dcc12e8398187a3af3848357231ad07a03f
calvean <garnetsharara@gmail.com>
2022-07-03 01:49:23 -0700

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  *main - prints combinations
- *
  *Return: 0 sucess 
  */
 int main(void)
 {
 	int i;
 	int j;
+	
 	for (i = 48; i <= 57; i++)
 	{
 		for (j = 48; j <= 57; j++)

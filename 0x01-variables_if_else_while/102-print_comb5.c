5bea2acfec6b999c3fcd6bc2747e79b361eba1f1
calvean <garnetsharara@gmail.com>
2022-07-03 04:26:29 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,51 +1,30 @@
 #include <stdio.h>
 /**
- * main - prints combination of 4
+ * main -print comb
  *
  * Return: 0 always
  */
 int main(void)
 {
-	int c, d, e, f, op1, op2;
+	int i, n;
 
-	c = d = e = f = 48;
-	while (f < 58)
+	for (i = 0; i <= 98; i++)
 	{
-		e = 48;
-		while (e < 58)
+		for (n = i +1; n <= 99; n++)
 		{
-			d = 48;
-			while (d < 58)
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(32);
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			/*my condition*/
+			if (i / 10 != 9 || i % 10 != 8)
 			{
-				c = 48;
-				while (c < 58)
-				{
-					op1 = (f * 10) + e;
-					op2 = (d * 10) + c;
-					if (op1 < op2)
-					{
-						putchar(f);
-						putchar(e);
-						putchar(' ');
-						putchar(d);
-						putchar(c);
-						if (f == 57 && e == 56 && d == 57 && c == 57)
-						{
-							break;
-						}
-						putchar(',');
-						putchar(' ');
-					}
-					c++;
-				}
-				d++;
+				putchar(44);
+				putchar(32);
 			}
-			e++;
 		}
-		f++;
 	}
-	putchar('\n');
-
+	putchar(10);
 	return (0);
 }
-

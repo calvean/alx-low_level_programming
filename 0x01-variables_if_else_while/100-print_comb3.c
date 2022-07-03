a99a559d74b7581a6ce968f1f2e8d2498d8ff15b
calvean <garnetsharara@gmail.com>
2022-07-03 01:37:21 -0700

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,13 +17,13 @@ int main(void)
 			{
 				putchar(i);
 				putchar(j);
+
+				if (i == 56 && j == 57)
+					continue;
+
+				putchar(44);
 				putchar(32);
-				/*
-				if (j <= 57)
-					putchar(44);
-					*/
 			}
-			putchar(44);
 		}
 	}
 	putchar(10);

d3fdfc6d3f45c9a067ded8c8a4fc131e95000c6f
calvean <garnetsharara@gmail.com>
2022-07-05 15:29:23 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - prints lower case
+ *
+ * Return: 0 if lower, 1 if upper
+ */
+int _islower(int c)
+{
+	int c;
+
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

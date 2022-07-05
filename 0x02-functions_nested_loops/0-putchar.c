3fada999948dc8e1d1fa1d6cbadd7febb9d82a5d
calvean <garnetsharara@gmail.com>
2022-07-05 13:55:24 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main prints putchar
+ * main - prints putchar
  *
  * Return: 0 always
  */
@@ -9,7 +9,7 @@ int main(void)
 	char putch[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	unsigned int c;
 
-	for (c =0; c < sizeof(putch); c++)
+	for (c = 0; c < sizeof(putch); c++)
 	{
 		_putchar(putch[c]);
 	}

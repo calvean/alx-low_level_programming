93db3d3b7e3770583f5661d6574e865735635f48
calvean <garnetsharara@gmail.com>
2022-07-05 14:03:28 -0700

    alpphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - print alphabet
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		_putchar(c);
+	}
+	_putchar(10);
+	return (0);
+}

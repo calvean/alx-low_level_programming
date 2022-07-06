5b14855c3290df2501eb859daa38943a5f2fd118
calvean <garnetsharara@gmail.com>
2022-07-07 00:50:20 +0200

    test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,40 @@
-#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
- * main - test function that print if interger is positive or negative
+ * positive_or_negative - checkes if its positive or negative
+ * main - prints output dependingon inpts
  *
- * Return: 0
+ *@i: integer
+ * Return: 0 if sucessful
  */
+int positive_or_negative(i)
+{
+	int n;
+
+	srand(time(0));
+	n = i;
+	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+
+	return (0);
+}
+/**
+* main - print interher
+*
+* Return: 0 always
+*/
 int main(void)
 {
 	int i;
 
-	i = 98;
-	positive_or_negative(i);
+	i = 0;
+	positve_or_negative(i);
 
 	return (0);
 }

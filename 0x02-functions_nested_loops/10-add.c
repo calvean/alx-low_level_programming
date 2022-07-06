09ece13403deddae0da7c4ad986eb60b8eabb63b
calvean <garnetsharara@gmail.com>
2022-07-06 11:53:01 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds to intergers
+ *
+ * @a: first interger
+ * @b: 2nd integer
+ * Return: 0 always
+ */
+int add(int, int)
+{
+	printf("%d", (a + b));
+	return (0);
+}

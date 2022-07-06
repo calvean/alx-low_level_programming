599baab9b9fcea0a48be3e9087ec915ff3c8a7ce
calvean <garnetsharara@gmail.com>
2022-07-06 11:43:46 -0700

    corrected main hearder

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ int print_sign(int c);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void time_table(void);
+void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_time_table(int n);

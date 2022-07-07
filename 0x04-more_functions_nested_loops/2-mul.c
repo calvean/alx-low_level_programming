9075a41f53b37374f98a7e2226dca0cb20278240
calvean <garnetsharara@gmail.com>
2022-07-07 23:32:29 +0200

    prod

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies 2 intergers
+ *
+ * @a: int of function
+ * @b: integer
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}

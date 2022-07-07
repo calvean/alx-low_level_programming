4f5dbc272dcfede900b31b9e7fb619c7828082b7
calvean <garnetsharara@gmail.com>
2022-07-07 11:13:29 +0200

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -11,30 +8,9 @@
 int main(void)
 {
 	int i;
-	/* whitespace*/
-	i = 98;
+	
+	i = 0;
 	positive_or_negative(i);
-	return (0);
-}
-/**
- * positive_or_negative - checkes if its positive or negative
- *
- *@i: integer
- * Return: 0 if sucessful
- */
-int positive_or_negative(i)
-{
-	int n;
-	/* whitespace */
-	srand(time(0));
-	n = i;
-	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	if (n == 0)
-		printf("%d is zero\n", n);
-	if (n < 0)
-		printf("%d is negative\n", n);
-	/* whitespace */
+	
 	return (0);
 }
---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints output dependingon inpts
+ *
+ * Return: 0 if sucessful
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+
+	return (0);
+}

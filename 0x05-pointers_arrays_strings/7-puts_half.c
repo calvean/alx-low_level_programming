65da5e44f1af20b52146d85010842882e6d548e2
calvean <garnetsharara@gmail.com>
2022-07-11 23:30:16 +0200

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* puts_half - prints half string
+*
+*@str: string
+* Return: void
+*/
+void puts_half(char *str)
+{
+	int n = strlen(str);
+	int i;
+
+	if ((n - 1) / 2 == 0)
+	{
+		for (i = 0; i < n / 2; i++)
+		{
+			printf("%c", str[i]);
+		}
+	}
+	else
+	{
+		for (i = n / 2; i < n; i++)
+		{
+			printf("%c", str[i]);
+		}
+	}
+	printf("\n");
+}

6e47e1c7b540cbb3196df7de2e36106745755fb3
calvean <garnetsharara@gmail.com>
2022-07-14 15:36:56 +0200

    string copy

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat- check the code
+ *
+ * @dest: destination
+ * @src: source
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	i = j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while ((dest[i++] = src[j++]) != '\0')
+		;
+	return (dest);
+}	

e49de82951e31bc3b550c579fd47ef1ecf652ecb
calvean <garnetsharara@gmail.com>
2022-08-18 22:18:17 +0200

    fixed return type

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,18 +5,18 @@
  * pop_listint - deletes the head node of a listint_t linked list
  * @head: pointer to pointer to the beginning of the list
  *
- * Return: the head node's data
+ * Return: the head node's data (n).
  */
 int pop_listint(listint_t **head)
 {
 	listint_t *first;
-	int i;
+	int n;
 
 	if (head == NULL || *head == NULL)
 		return (0);
 	first = *head;
 	*head = first->next;
-	i = first->i;
+	n = first->n;
 	free(first);
-	return (i);
+	return (n);
 }

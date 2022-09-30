4394f5b7a3539a4a09ba398b51205749ef2c7507
calvean <garnetsharara@gmail.com>
2022-09-30 11:58:48 +0200

    fized

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - print all the elements of a dlistint_t list
+ * @h: pointer to head
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes;
+
+	for (nodes = 0; h != NULL; nodes++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nodes);
+}

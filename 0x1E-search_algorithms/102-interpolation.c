a721f6c1be38586af5d1ad329a405ddb6d5913a7
calvean <garnetsharara@gmail.com>
2023-01-27 23:32:48 +0200

    advanced

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+/**
+  * interpolation_search - Searches for a value in a sorted array
+  *                        of integers using interpolation search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: number of elements in the array.
+  * @value: value to search for.
+  *
+  * Return: -1 if Value==NULL or not present, index otherwise
+  */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t i, left, right;
+
+	if (array == NULL)
+		return (-1);
+
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		i = left + (((double)(right - left) / (
+		array[right] - array[left])) * (value - array[left]));
+		if (i < size)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		else
+		{
+			printf("Value checked array[%ld] is out of range\n", i);
+			break;
+		}
+
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+
+	return (-1);
+}
